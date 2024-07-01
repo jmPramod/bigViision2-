e5c99911470e584402cea306ef1d5f72a7ffbaee
Rohit <gaikwadr575@gmail.com>
2024-07-01 16:14:52 +0530

    color combination change

--- src/app/componets/Footer/Footer.tsx
+++ src/app/componets/Footer/Footer.tsx
@@ -1,29 +1,29 @@
 /* eslint-disable react/no-unescaped-entities */
-'use client';
-import Image from 'next/image';
-import React from 'react';
-import { AiOutlineWhatsApp } from 'react-icons/ai';
-import { AiFillInstagram } from 'react-icons/ai';
-import { AiFillFacebook } from 'react-icons/ai';
-import Link from 'next/link';
+"use client";
+import Image from "next/image";
+import React from "react";
+import { AiOutlineWhatsApp } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
+import { AiFillFacebook } from "react-icons/ai";
+import Link from "next/link";
 const Footer = () => {
   return (
     <div className="  w-full">
-      <footer className=" dark:bg-gray-900  w-full bg-[#b1b1ff]">
+      <footer className=" dark:bg-gray-900  w-full bg-[#494959]">
         <div className="mx-auto w-full  p-4 py-6 lg:py-8">
           <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
               <Image
                 className="rounded  "
-                src={'/image/logo.jpg'}
+                src={"/image/logo.jpg"}
                 width={150}
                 height={150}
                 alt="logo"
               />
             </div>
-            <div className="md:w-[30%] w-full">
-              <h2 className="mb-6 text-lg font-semibold text-gray-900 uppercase dark:text-white">
-                ADDRESS
+            <div className="md:w-[30%] w-full text-white">
+              <h2 className="mb-6 text-lg font-semibold uppercase dark:text-white">
+                ADDRESS:
               </h2>
               <ul className=" font-bold">
                 <li className="mb-4">
@@ -34,10 +34,10 @@ const Footer = () => {
             </div>
             <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-1">
               <div>
-                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                <h2 className="mb-6 text-sm font-semibold uppercase text-white dark:text-white">
                   Legal
                 </h2>
-                <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                <ul className="text-white dark:text-gray-400 font-medium">
                   <li className="mb-4">
                     <a href="#" className="hover:underline">
                       Privacy Policy
@@ -54,9 +54,9 @@ const Footer = () => {
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <div className="sm:flex sm:items-center sm:justify-between">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-              © 2023{' '}
-              <a href="https://flowbite.com/" className="hover:underline">
+            <span className="text-sm text-white sm:text-center dark:text-gray-400">
+              © 2024{" "}
+              <a href="#" className="hover:underline">
                 Bigviision Printers™
               </a>
               . All Rights Reserved.
@@ -64,9 +64,9 @@ const Footer = () => {
             <div className="flex mt-4 sm:justify-center sm:mt-0">
               <a
                 href="#"
-                className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+                className="bg-white hover:text-gray-900 dark:hover:text-white"
               >
-                <AiFillFacebook size={30} />
+                <AiFillFacebook size={30} color="#316FF6" />
                 <span className="sr-only">Facebook page</span>
               </a>
 
--- src/app/componets/NavBar/Navbar.tsx
+++ src/app/componets/NavBar/Navbar.tsx
@@ -1,41 +1,41 @@
-'use client';
+"use client";
 
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
-import { AiOutlineMenu } from 'react-icons/ai';
-import React, { useState } from 'react';
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { AiOutlineMenu } from "react-icons/ai";
+import React, { useState } from "react";
 import {
   Sheet,
   SheetContent,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from '@/components/ui/sheet';
-import { useRouter } from 'next/navigation';
-import { MobileNav } from './MobileNav';
+} from "@/components/ui/sheet";
+import { useRouter } from "next/navigation";
+import { MobileNav } from "./MobileNav";
 const Navbar = () => {
   const router = useRouter();
   const [toggleMenu, setToggleMenu] = useState(false);
   return (
-    <nav className="h-16 w-full bg-[#1976d2] flex items-center justify-between px-5">
+    <nav className="h-16 w-full bg-[#0e1111] flex items-center justify-between px-5">
       {/* logo */}
       <div>
         <Image
           className="rounded"
-          src={'/image/logo.jpg'}
+          src={"/image/logo.jpg"}
           width={130}
           height={130}
           alt="logo"
-          onClick={() => router.push('/')}
+          onClick={() => router.push("/")}
         />
       </div>
 
       {/* menu */}
       <div className="hidden items-center justify-center gap-4 md:flex ">
-        <Button variant={'custom1'} onClick={() => router.push('/')}>
+        <Button variant={"custom1"} onClick={() => router.push("/")}>
           Home
         </Button>
-        <Button variant={'custom1'} onClick={() => router.push('/contact')}>
+        <Button variant={"custom1"} onClick={() => router.push("/contact")}>
           Contact
         </Button>
       </div>
